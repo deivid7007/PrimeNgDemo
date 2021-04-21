a4de1c1fece275482246d01efe703d6d58bef1cc
Deivid Mladenov <dmladenov@zingasoft.com>
2021-04-21 16:50:08 +0300

    feat: added accessibility via dom manipulation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import * as en from '../assets/lang/lang-en.json'
 import * as de from '../assets/lang/lang-de.json'
 
@@ -7,14 +7,25 @@ import * as de from '../assets/lang/lang-de.json'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'prime-ng-demo';
   checkedWifi: boolean = true;
   checkedSwitch: boolean = true;
   currentLang: any;
+  @ViewChild('toggleButtonDiv') private toggleButtonRef: ElementRef<HTMLElement>;
+
+  ngAfterViewInit() {
+    console.log(this.toggleButtonRef);
+    const parentElement = this.toggleButtonRef.nativeElement;
+    console.log(parentElement);
+    const firstChild = parentElement.children[0];
+    const firstImage = parentElement.querySelector("p-togglebutton div");
+    firstImage.setAttribute('aria-label', 'wifi-toggle-button');
+    console.log(firstImage);
+  }
 
   ngOnInit() {
-    this.currentLang = de;
+    this.currentLang = en;
     this.currentLang = this.currentLang.default;
 
     console.log(this.currentLang.ToggleButton);
